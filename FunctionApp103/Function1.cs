286dc014c5b0f2933ae28cc650527ed29e000c32
Shyju Krishnankutty <connectshyju@gmail.com>
2023-08-30 07:24:20 -0700

    Test commit

--- FunctionApp103/Function1.cs
+++ FunctionApp103/Function1.cs
@@ -22,7 +22,7 @@ namespace FunctionApp103
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            response.WriteString("Welcome to Azure Functions 20230830!");
 
             return response;
         }
